05984575d99456750d9b11ed1c694cd39efef364
ROBIN <robincuetcse@gmail.com>
2021-01-30 22:25:13 +0600

    Zoom Clone Done By Robin

--- public/script.js
+++ public/script.js
@@ -2,9 +2,9 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined,{
   path: '/peerjs',
-  host: 'localhost',
-  port: '443'
-})
+  host: '/localhost',
+  port: '3030'
+});
 let myVideoStream;
 const myVideo = document.createElement('video')
 myVideo.muted = true;
